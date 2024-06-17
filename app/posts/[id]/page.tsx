15b6f801e3c98522e40dec4d84961601e194f7b2
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2024-06-17 21:43:27 +0900

    feat(design): スタイル反映

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getPost } from '@/app/posts/[id]/data';
+import { format } from 'date-fns';
 
 export default async function Page({
   params: { id },
@@ -8,9 +9,15 @@ export default async function Page({
   const post = await getPost(id);
 
   return (
-    <div>
-      <h1>{post.title.rendered}</h1>
+    <article className="prose prose-zinc dark:prose-invert dark:prose-pre:border">
+      <div className="not-prose">
+        <h1 className="font-bold text-4xl mb-4">{post.title.rendered}</h1>
+        <p className="text-muted-foreground">
+          {format(new Date(post.date), 'yyyy年MM月dd日')}
+        </p>
+      </div>
+
       <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
-    </div>
+    </article>
   );
 }
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,20 +1,60 @@
 import { getPosts } from '@/app/posts/data';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import { format } from 'date-fns';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default async function Page() {
   const posts = await getPosts();
 
   return (
-    <div>
-      <h1>記事一覧</h1>
+    <>
+      <h1 className="font-bold text-xl mb-6">記事一覧</h1>
 
-      {posts.map((post) => (
-        <div key={post.id}>
-          <h2>
-            <Link href={`/posts/${post.id}`}>{post.title.rendered}</Link>
-          </h2>
-        </div>
-      ))}
-    </div>
+      <div className="grid grid-cols-4 gap-4">
+        {posts.map((post) => (
+          <Card key={post.id} className="relative">
+            <CardHeader>
+              <CardTitle>
+                <Link href={`/posts/${post.id}`}>
+                  {post.title.rendered}
+                  <span className="absolute inset-0"></span>
+                </Link>
+              </CardTitle>
+              <p className="text-muted-foreground">
+                {format(new Date(post.date), 'yyyy年MM月dd日')}
+              </p>
+            </CardHeader>
+            <CardContent>
+              <p
+                className={cn(
+                  post.jetpack_featured_media_url
+                    ? 'line-clamp-3'
+                    : 'line-clamp-6'
+                )}
+                dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+              />
+
+              {post.jetpack_featured_media_url && (
+                <Image
+                  src={post.jetpack_featured_media_url}
+                  alt=""
+                  width={800}
+                  height={400}
+                  className="aspect-video object-cover object-center rounded-md mt-6 bg-muted"
+                />
+              )}
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 }
